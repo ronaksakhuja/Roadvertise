c30d75afbd5a839a7ce0c62f1f9ca6be1abd288a
ronaksakhuja@gmail.com <ronaksakhuja@gmail.com>
2017-12-03 02:20:31 +0530

    Final

--- win_bid.php
+++ win_bid.php
@@ -1,6 +1,10 @@
 <?php
+$max_bid=$_REQUEST['maxbid'];
+$winner=$_REQUEST['winner'];
+$time=$_REQUEST['time'];
+$con = mysqli_connect('localhost', 'root', '', 'rh3');
+$query="UPDATE max_bid SET max_bid='$max_bid',winner='$winner' WHERE time='$time'";
+$result=$con->query($query);
 
-  $con = mysqli_connect('localhost', 'root', '', 'rh3');
-$query="UPDATE TABLE max_bid SET max_bid='$max_bid' AND winner='$winner' WHERE time='$time'";
 
 ?>
